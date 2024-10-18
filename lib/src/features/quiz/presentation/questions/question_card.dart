669505519566f7175c66221a69b187b6deb45c7f
Rashid Holder <rashid.m.holder@gmail.com>
2024-10-18 09:26:47 -0400

    Fixed layout issue with QuestionCard

--- lib/src/features/quiz/presentation/questions/question_card.dart
+++ lib/src/features/quiz/presentation/questions/question_card.dart
@@ -9,46 +9,59 @@ class QuestionCard extends ConsumerWidget {
   final Question question;
   final int currentQuestionNumber;
 
-  const QuestionCard(
-      {super.key, required this.question, required this.currentQuestionNumber});
+  const QuestionCard({super.key, required this.question, required this.currentQuestionNumber});
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    List<String> answers;
-
+  List<String> _generateAnswers(Question question) {
     if (question.type == 'boolean') {
-      answers = ['True', 'False'];
-    } else {
-      answers = [...question.incorrectAnswers, question.correctAnswer]
-        ..shuffle();
+      return ['True', 'False'];
     }
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Center(
-            child: Text(
-              "#${currentQuestionNumber.toString()}",
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Text(
-            question.question,
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-          ),
-          const SizedBox(height: 20),
+
+    return [...question.incorrectAnswers, question.correctAnswer]..shuffle();
+  }
+
+  Widget _generateAnswersList(WidgetRef ref, List<String> answers) {
+    return Flexible(
+        flex: 4,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
           ...answers.map((answer) => AnswerButton(
                 answer: answer,
                 onPressed: () {
                   if (answer == question.correctAnswer) {
                     ref.read(scoreNotifierProvider.notifier).incrementScore();
                   }
-                  ref
-                      .read(currentQuestionIndexProvider.notifier)
-                      .nextQuestion();
+                  ref.read(currentQuestionIndexProvider.notifier).nextQuestion();
                 },
               )),
+        ]));
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<String> answers = _generateAnswers(question);
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Flexible(
+              flex: 1,
+              child: Column(
+                children: [
+                  Center(
+                    child: Text(
+                      "#${currentQuestionNumber.toString()}",
+                      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(
+                    question.question,
+                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                  ),
+                ],
+              )),
+          const SizedBox(height: 20),
+          _generateAnswersList(ref, answers)
         ],
       ),
     );
